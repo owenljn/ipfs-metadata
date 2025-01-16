21f79e61e66af953069318925df1fe17ae5d576e
Owen Lu <owenljn@gmail.com>
2025-01-15 20:17:46 -0500

    code cleanup

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -73,6 +73,7 @@ resource "aws_subnet" "public_subnets" {
   count                   = length(var.public_subnets)
   vpc_id                  = aws_vpc.this.id
   cidr_block              = var.public_subnets[count.index]
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
   map_public_ip_on_launch = true
 
   tags = {
@@ -84,7 +85,7 @@ resource "aws_subnet" "public_subnets" {
 # Private Subnets
 ###################################
 resource "aws_subnet" "private_subnets" {
-  count             = length(var.private_subnets)  # or 2 if you want exactly 2
+  count             = length(var.private_subnets)
   vpc_id            = aws_vpc.this.id
   cidr_block        = element(var.private_subnets, count.index)
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
